a077bd847313658561935ba4e820c0bbb15622b9
td2466 <td2466@columbia.edu>
2016-03-27 21:44:05 -0400

    added login

--- webserver/server.py
+++ webserver/server.py
@@ -193,14 +193,21 @@ def add():
   name = request.form['name']
   g.conn.execute('INSERT INTO test VALUES (NULL, ?)', name)
   return redirect('/')
-
-
-@app.route('/login')
+  
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    abort(401)
-    this_is_never_executed()
-
-
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+    
 if __name__ == "__main__":
   import click
 
