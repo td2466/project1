02cdcbcc5affc6fb178bc8e59e1f278136ff38e6
td2466 <td2466@columbia.edu>
2016-03-28 01:20:16 -0400

    enable debug

--- webserver/server.py
+++ webserver/server.py
@@ -236,7 +236,7 @@ if __name__ == "__main__":
 
     HOST, PORT = host, port
     print "running on %s:%d" % (HOST, PORT)
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+    app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
 
 
   run()
