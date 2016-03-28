156c8698d67a6e100f31371f91300288b0c65d83
td2466 <td2466@columbia.edu>
2016-03-27 21:46:49 -0400

    updated login.01

--- webserver/server.py
+++ webserver/server.py
@@ -206,7 +206,7 @@ def login():
             session['logged_in'] = True
             flash('You were logged in')
             return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+    return render_template('index.html', error=error)
     
 if __name__ == "__main__":
   import click
